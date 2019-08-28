23dd6759f7079d7bf3fef9f665f3f4529782ac96
insomnyak <rene@insomnyak.com>
2019-08-28 09:46:26 -0400

    first commit

---
+++ src/main/java/com/company/rentalstorerestwebservicegroupproject/RentalStoreRestWebServiceGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.company.rentalstorerestwebservicegroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalStoreRestWebServiceGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalStoreRestWebServiceGroupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/rentalstorerestwebservicegroupproject/RentalStoreRestWebServiceGroupProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.rentalstorerestwebservicegroupproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentalStoreRestWebServiceGroupProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
